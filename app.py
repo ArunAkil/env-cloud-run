6e16c08654d358d38b5ce98479920237c685973c
ArunAkil <90335906+ArunAkil@users.noreply.github.com>
2022-02-03 14:30:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,4 +23,4 @@ def my_form_post():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8090)))
